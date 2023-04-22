e914762fdbc5d0f78bdd7eb202481bb5e1faa0c3
Илья <ilya19xx2014@gmail.com>
2023-04-22 23:12:38 +0300

    feat: Добавил обработчики аккаунтов на просмотр

--- hippodrome/mainapp/views.py
+++ hippodrome/mainapp/views.py
@@ -16,6 +16,8 @@ def LoginUser(request):
     pass
 def LogoutUser(request):
     pass
+
+
 def RacesList(request):
     objs = Race.objects.all()
     elem_table = []
@@ -88,9 +90,9 @@ def HorsesList(request):
                 'link': ''
             },
             {
-                'type': 'text',
+                'type': 'link',
                 'value': o.owner.user.__str__(),
-                'link': o.get_absolute_url()
+                'link': o.owner.get_absolute_url()
             },
         ])
     side_menu = []
@@ -201,14 +203,18 @@ def RepportsList(request):
     }
     return render(request, 'mainapp/reports.html', context=context)
 
+
 def RaceId(request, record):
     rec = get_object_or_404(Race, id=record)
+
     now = datetime.now()
+    tb = datetime(year=rec.date.year, month=rec.date.month, day=rec.date.day, hour=rec.time_begin.minute,
+                  minute=rec.time_begin.minute, second=0)
+    te = datetime(year=rec.date.year, month=rec.date.month, day=rec.date.day, hour=rec.time_end.minute,
+                  minute=rec.time_end.minute, second=0)
+
 
     status = ''
-    tb = datetime(year=rec.date.year, month=rec.date.month, day=rec.date.day, hour=rec.time_begin.minute, minute=rec.time_begin.minute, second=0)
-    te = datetime(year=rec.date.year, month=rec.date.month, day=rec.date.day, hour=rec.time_end.minute, minute=rec.time_end.minute, second=0)
-
     if now >= tb :
         status = 'Завершен'
     if now < tb :
@@ -222,6 +228,7 @@ def RaceId(request, record):
         side_menu = visitors_menu
     else:
         side_menu = admin_menu
+
     context = {
         'title': 'Заезд: '+rec.title,
         'title_race': rec.title,
@@ -245,12 +252,178 @@ def RaceId(request, record):
         'edit_url': reverse('race_edit', args=[rec.id])
     }
     return render(request, 'mainapp/race.html', context=context)
-def JockeyId(request):
-    pass
-def HorseId(request):
-    pass
-def OwnerId(request):
-    pass
+def JockeyId(request, record):
+    if request.user.is_superuser == False:
+        side_menu = visitors_menu
+    else:
+        side_menu = admin_menu
+
+    rec = get_object_or_404(Jockey, id=record)
+
+    d = [
+        {'type':'text', 'link':'', 'desc':'ФИО', 'value': rec.__str__()},
+        {'type':'text', 'link':'', 'desc':'Категория', 'value': rec.category},
+        {'type':'text', 'link':'', 'desc':'Телефон', 'value': rec.user.phone},
+        {'type':'text', 'link':'', 'desc':'Электронная почта', 'value': rec.user.user.email},
+        {'type':'text', 'link':'', 'desc':'Город', 'value': rec.user.city.__str__()},
+        {'type':'text', 'link':'', 'desc':'Дата рождения', 'value': rec.user.birth},
+    ]
+
+    desc_table = ['Дата', 'Событие', 'Скакун', 'Дистанция', 'Результат', 'Время']
+    elem_table = []
+    couples = Couple.objects.filter(jockey=rec.id)
+    for c in couples:
+        elem_table.append([
+            {
+                'type':'text',
+                'value':c.race.date,
+                'link':''
+            },
+            {
+                'type': 'link',
+                'value': c.race.__str__(),
+                'link': c.race.get_absolute_url()
+            },
+            {
+                'type': 'link',
+                'value': c.horse.__str__(),
+                'link': c.horse.get_absolute_url()
+            },
+            {
+                'type': 'text',
+                'value': c.race.distance,
+                'link': ''
+            },
+            {
+                'type': 'text',
+                'value': c.result,
+                'link': ''
+            },
+            {
+                'type': 'text',
+                'value': c.time,
+                'link': ''
+            },
+        ])
+    context = {
+        'title': 'Наездник: ' + rec.__str__(),
+        'account_name': rec.__str__(),
+        'side_menu': side_menu,
+        'punkt_selected': -1,
+        'is_login': request.user.is_authenticated,
+        'is_self_or_admin': request.user.is_superuser or request.user.id == rec.id,
+        'record':d,
+        'title_table':'Результаты заездов',
+        'desc_table':desc_table,
+        'elem_table':elem_table,
+        'photo': rec.user.photo.url,
+        'edit_url': reverse('jockey_edit', args=[rec.id])
+    }
+    return render(request, 'mainapp/acc_horse_jokey.html', context=context)
+def HorseId(request, record):
+    if request.user.is_superuser == False:
+        side_menu = visitors_menu
+    else:
+        side_menu = admin_menu
+
+    rec = get_object_or_404(Horse, id=record)
+
+    d = [
+        {'type':'text', 'link':'', 'desc':'Кличка', 'value': rec.__str__()},
+        {'type':'text', 'link':'', 'desc':'Масть', 'value': rec.mast},
+        {'type':'text', 'link':'', 'desc':'Возраст', 'value': rec.age},
+        {'type':'link', 'link':rec.owner.get_absolute_url(), 'desc':'Владелец', 'value': rec.owner.__str__()},
+    ]
+
+    desc_table = ['Дата', 'Событие', 'Жокей', 'Дистанция', 'Результат', 'Время']
+    elem_table = []
+    couples = Couple.objects.filter(horse=rec.id)
+    for c in couples:
+        elem_table.append([
+            {
+                'type':'text',
+                'value':c.race.date,
+                'link':''
+            },
+            {
+                'type': 'link',
+                'value': c.race.__str__(),
+                'link': c.race.get_absolute_url()
+            },
+            {
+                'type': 'link',
+                'value': c.jockey.__str__(),
+                'link': c.jockey.get_absolute_url()
+            },
+            {
+                'type': 'text',
+                'value': c.race.distance,
+                'link': ''
+            },
+            {
+                'type': 'text',
+                'value': c.result,
+                'link': ''
+            },
+            {
+                'type': 'text',
+                'value': c.time,
+                'link': ''
+            },
+        ])
+    context = {
+        'title': 'Скакун: ' + rec.__str__(),
+        'account_name': rec.__str__(),
+        'side_menu': side_menu,
+        'punkt_selected': -1,
+        'is_login': request.user.is_authenticated,
+        'is_self_or_admin': request.user.is_superuser,
+        'record':d,
+        'title_table':'Результаты заездов',
+        'desc_table':desc_table,
+        'elem_table':elem_table,
+        'photo': rec.photo.url,
+        'edit_url': reverse('horse_edit', args=[rec.id])
+    }
+    return render(request, 'mainapp/acc_horse_jokey.html', context=context)
+def OwnerId(request, record):
+    if request.user.is_superuser == False:
+        side_menu = visitors_menu
+    else:
+        side_menu = admin_menu
+
+    rec = get_object_or_404(Owner, id=record)
+
+    d = [
+        {'type':'text', 'link':'', 'desc':'ФИО', 'value': rec.__str__()},
+        {'type':'text', 'link':'', 'desc':'Телефон', 'value': rec.user.phone},
+        {'type':'text', 'link':'', 'desc':'Электронная почта', 'value': rec.user.user.email},
+        {'type':'text', 'link':'', 'desc':'Город', 'value': rec.user.city.__str__()},
+        {'type':'text', 'link':'', 'desc':'Дата рождения', 'value': rec.user.birth},
+    ]
+
+    couples = Couple.objects.filter(horse=rec.id)
+    horses = Horse.objects.filter(owner=record)
+    couples = []
+    for h in horses:
+        for c in Couple.objects.filter(horse=h.id):
+            couples.append(c)
+
+    context = {
+        'title': 'Собственник: ' + rec.__str__(),
+        'account_name': rec.__str__(),
+        'side_menu': side_menu,
+        'punkt_selected': -1,
+        'is_login': request.user.is_authenticated,
+        'is_self_or_admin': request.user.is_superuser or request.user.id == rec.id,
+        'record':d,
+        'photo': rec.user.photo.url,
+        'edit_url': reverse('owner_edit', args=[rec.id]),
+        'horses':horses,
+        'couples':couples
+    }
+    return render(request, 'mainapp/acc_sobstvennik.html', context=context)
+
 
 def RaceEdit(request, record):
     if request.user.is_superuser == False:
